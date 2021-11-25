baf4454f0166f2ff93f1f852560ff2ecc1049d6f
dimipak <jimpak13@gmail.com>
2021-11-25 14:57:30 +0200

    fix

--- machine/bootstrap/before_install.sh
+++ machine/bootstrap/before_install.sh
@@ -7,8 +7,8 @@ mkdir app
 
 cd app
 
-touch mytestfile.txt
+touch mytestfile153.txt
 
-sudo aws s3 cp s3://webcv-configurations/backend/ /home/ubuntu/app -r
+#sudo aws s3 cp s3://webcv-configurations/backend/ /home/ubuntu/app -r
 
 #docker-compose -f /home/ubuntu/app/docer-compose.yml
