416f1081fc4a05089157c9c9763861b13776fdf1
dimipak <jimpak13@gmail.com>
2021-11-25 14:43:51 +0200

    test

--- machine/bootstrap/before_install.sh
+++ machine/bootstrap/before_install.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
-#sudo aws s3 cp s3://webcv-configurations/backend/ /home/ubuntu/app -r
-#
+cd /home/ubuntu
+sudo rm -rf app
+
+mkdir app
+
+cd app
+
+touch mytestfile.txt
+
+sudo aws s3 cp s3://webcv-configurations/backend/ /home/ubuntu/app -r
+
 #docker-compose -f /home/ubuntu/app/docer-compose.yml
